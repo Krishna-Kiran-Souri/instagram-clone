eb622888a363722ff32595b73ca776b280391d9c
Krishna-Kiran-Souri <krishnakiransouri.infy@gmail.com>
2021-01-11 23:52:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-lcpebg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-lcpebg)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import { Layout } from "./layout";
+
+export default function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
