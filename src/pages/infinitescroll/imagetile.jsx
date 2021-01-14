8278899789ef73d2c0792f4a76d70427876453e9
Krishna-Kiran-Souri <krishnakiransouri.infy@gmail.com>
2021-01-14 22:14:49 +0530

    1 file modified

--- src/pages/infinitescroll/imagetile.jsx
+++ src/pages/infinitescroll/imagetile.jsx
@@ -27,7 +27,7 @@ export const ImageTiles = props => {
   const fetchPhotos = async pageNumber => {
     const ACCESS_KEY = "1nzzDPNsXTV-rn7FHQbFXbPWdgECj5Mp5zG1K5_-gHY";
     const res = await fetch(
-      `https://api.unsplash.com/search/photos?query=instagram&client_id=${ACCESS_KEY}&page=${pageNumber}&per_page=12`
+      `https://api.unsplash.com/search/photos?query=instagram&client_id=${ACCESS_KEY}&page=${pageNumber}&per_page=21`
     );
     const data = await res.json();
     // console.log(data);
