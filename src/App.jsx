23792e01a23863c43262b10310f62c1fec07ce16
Krishna-Kiran-Souri <krishnakiransouri.infy@gmail.com>
2021-01-18 00:03:16 +0530

    4 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import "./style.css";
 import { Layout } from "./layout";
 import { ImageTiles } from "./pages/infinitescroll/imagetile.jsx";
-
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles(theme => ({
+  feed: {
+    paddingTop: 20,
+    marginTop: 20,
+    width: "100vw",
+    height: "100vh",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "row"
+  }
+}));
 export default function App() {
+  const classes = useStyles();
   return (
     <div>
       <Layout />
-      <ImageTiles />
+      <div className={classes.feed}>
+        <ImageTiles />
+      </div>
     </div>
   );
 }
--- src/pages/infinitescroll/imagetile.jsx
+++ src/pages/infinitescroll/imagetile.jsx
@@ -1,10 +1,10 @@
 import { useImageService } from "../../service/imageservice.js";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./imagetile.css";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-
+import CircularIndeterminate from "../../utl/components/circularspinner.jsx";
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
@@ -12,17 +12,13 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "space-around",
     overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
-    marginTop: "20px",
     height: "100vh"
   },
   gridList: {
-    width: 500,
-    height: 700,
     position: "realtive",
-    marginTop: "50px",
-    paddingTop: "50px",
-    width: "100vw",
-    overFlowY: "hidden"
+    paddingTop: 10,
+    overFlowY: "hidden",
+    height: "100vh"
   }
 }));
 
@@ -30,32 +26,50 @@ export const ImageTiles = props => {
   const classes = useStyles();
   const [images, setImages] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
-
+  const [loading, setLoading] = useState(false);
   const fetchPhotos = async pageNumber => {
     const ACCESS_KEY = "1nzzDPNsXTV-rn7FHQbFXbPWdgECj5Mp5zG1K5_-gHY";
     const res = await fetch(
       `https://api.unsplash.com/search/photos?query=instagram&client_id=${ACCESS_KEY}&page=${pageNumber}&per_page=21`
     );
     const data = await res.json();
+
     // console.log(data);
     let temp = data.results.map((img, index) => {
       let row = 1;
       let col = 1;
-      if (index === 0) {
-        col = 3;
-        row = 2;
-      }
-
       return { url: img.urls.small, col: col, row: row };
     });
     console.log(temp);
     setImages(p => [...p, ...temp]);
+    setLoading(false);
   };
+  const pageEnd = useRef();
   useEffect(() => {
     fetchPhotos(pageNumber);
   }, [pageNumber]);
+
+  useEffect(() => {
+    if (loading) {
+      const observer = new IntersectionObserver(
+        entries => {
+          console.log(entries);
+        },
+        { threshold: 1 }
+      );
+      observer.observe(pageEnd.current);
+    }
+  }, [loading]);
   const loadMore = () => {
     setPageNumber(prevpagenumber => prevpagenumber + 1);
+    setLoading(true);
+  };
+  const breakpointColumnsObj = {
+    default: 6,
+    1200: 3,
+    992: 3,
+    768: 2,
+    576: 1
   };
   // const imageTiles=
 
@@ -64,21 +78,24 @@ export const ImageTiles = props => {
     // <h1>{images.map(tile=>tile)}</h1>
     <div className={classes.root + " imagegrid"}>
       <GridList
-        cellHeight={160}
         className={classes.gridList + " grid-list"}
         cols={3}
+        ref={pageEnd}
+        onScroll={loadMore}
       >
         {images.map((tile, index) => (
           <GridListTile
             key={index}
             cols={tile.col || 1}
             rows={tile.row}
-            spacing={10}
+            spacing={1}
           >
             <img src={tile.url} alt={tile.title} />
           </GridListTile>
         ))}
       </GridList>
+
+      {loading ? <CircularIndeterminate /> : <></>}
     </div>
   );
 };
