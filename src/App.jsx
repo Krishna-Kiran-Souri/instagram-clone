f975b4d676464142ca7e9b65610cbacf1f71f8fc
Krishna-Kiran-Souri <krishnakiransouri.infy@gmail.com>
2021-01-17 21:49:37 +0530

    2 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import { Layout } from "./layout";
 import { ImageTiles } from "./pages/infinitescroll/imagetile.jsx";
+
 export default function App() {
   return (
     <div>
